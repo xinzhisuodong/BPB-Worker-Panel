a09fa7a9f08691beafd424ec6478f55f7efc191d
xinzhisuodong <162891518+xinzhisuodong@users.noreply.github.com>
2024-07-21 02:31:04 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -7,7 +7,7 @@ import { connect } from 'cloudflare:sockets';
 
 // How to generate your own UUID:
 // https://www.uuidgenerator.net/
-let userID = '89b3cbba-e6ac-485a-9481-976a0415eab9';
+let userID = 'c0599a4e-ddc4-4d60-9df2-e2e54fb101e9';
 
 // https://www.nslookup.io/domains/cdn.xn--b6gac.eu.org/dns-records/
 // https://www.nslookup.io/domains/cdn-all.xn--b6gac.eu.org/dns-records/
